8289336a53d9f7d98923ba7a2a0b51dfce14d3c6
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2023-07-10 12:43:31 +0530

    Start

---
+++ Intro/First.java
@@ -0,0 +1,27 @@
+package Intro;// Folder name inside which the program is wr
+
+import java.util.*;
+import java.lang.*;
+
+public class First {
+    public static void main(String[] arg) {
+        // System.out.println("Hello World");
+        // System.out.println("Enter two Numbers:");
+        // Scanner sc = new Scanner(System.in);
+
+        // int c;
+        // int b;
+        // int a;
+
+        // a = sc.nextInt();
+        // b = sc.nextInt();
+        // c = a + b;
+        // System.out.println("Addition of two Number is:" + c);
+
+        System.out.println("May I know whom I am Speaking with?");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Welcome back " + name);
+
+    }
+}
