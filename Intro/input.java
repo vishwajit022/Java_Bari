1ddec0fc7350723afbee364c47c70630c4fba8d8
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2023-07-18 02:34:29 +0530

    Create input.java

---
+++ Intro/input.java
@@ -0,0 +1,16 @@
+package Intro;
+
+import java.lang.*;
+import java.util.*;
+
+public class input {
+    public static void main(String arg[]) {
+        System.out.println("Enter two numbers");
+        Scanner sc = new Scanner(System.in);
+        int a, b, c;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = a + b;
+        System.out.println(c);
+    }
+}
