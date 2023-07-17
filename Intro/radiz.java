dacc3c92d7c2c31fc7e52dfa9bb89ef50c8dde7d
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2023-07-18 02:41:48 +0530

    go

---
+++ Intro/radiz.java
@@ -0,0 +1,16 @@
+package Intro;
+
+import java.util.*;
+
+public class radiz {
+    public static void main(String arg[]) {
+        Scanner sc = new Scanner(System.in);
+        sc.useRadix(2); // This converts binary to decimal
+        int x = sc.nextInt();
+        System.out.println(x);
+        // Input 1010
+        // Output 10
+        // 8421 concept
+
+    }
+}
