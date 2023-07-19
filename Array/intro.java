7ffac651578dae68fa20e38d1dbb2fa3f9cafc3f
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2023-07-19 19:25:14 +0530

    Create intro.java

---
+++ Array/intro.java
@@ -0,0 +1,20 @@
+package Array;
+
+import java.util.*;
+
+public class intro {
+
+    public static void main(String args[]) { // Ways to declare arrays
+        int A[] = new int[10];
+        int B[] = { 10, 3, 4, 56, 6 };
+        int C[];
+        C = new int[10];
+
+        // Tranversing an array
+
+        for (int i = 0; i <= A.length; i++) {
+            System.out.println(B[i]);
+        }
+
+    }
+}
