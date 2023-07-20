b0f8c7959209f534f58b69d1a7d67ac3cd4da5fa
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2023-07-21 02:41:35 +0530

    Create thisj.java

---
+++ OOPS/thisandsuper/thisj.java
@@ -0,0 +1,15 @@
+package OOPS.thisandsuper;
+
+class Rectangle {
+    int length;
+    int breadth;
+
+    Rectangle(int l, int b) {
+        this.length = l;
+        this.breadth = b;
+    }
+}
+
+public class thisj {
+
+}
