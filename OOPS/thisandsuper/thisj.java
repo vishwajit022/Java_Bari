c1949ce074a9a240906cb31c7e71a2965ec07298
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2023-07-22 01:38:08 +0530

    Update thisj.java

--- OOPS/thisandsuper/thisj.java
+++ OOPS/thisandsuper/thisj.java
@@ -4,12 +4,24 @@ class Rectangle {
     int length;
     int breadth;
 
-    Rectangle(int l, int b) {
-        this.length = l;
-        this.breadth = b;
+    Rectangle(int length, int breadth) {
+        this.length = length;
+        this.breadth = breadth;
+        // We use this keyword to refer to variable of the current class so if
+        // we have same var name the compiler doesn't get confused while assigning
+        // values
+    }
+
+    void display() {
+        System.out.println("Lenght:" + this.length);
+        System.out.println("Breadth:" + this.breadth);
     }
 }
 
 public class thisj {
+    public static void main(String arg[]) {
+        Rectangle r = new Rectangle(4, 5);
+        r.display();
+    }
 
 }
