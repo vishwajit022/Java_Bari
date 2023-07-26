f2ed216cca32b54f2b02745a3bef116655305773
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2023-07-26 22:13:16 +0530

    Update error.java

--- ErrorHandling/error.java
+++ ErrorHandling/error.java
@@ -6,6 +6,8 @@ public class error {
         int a, b, c;
         a = 5;
         b = 0;
+        // It can be use to avoid risk of exception while user is using program
+
         try {
             c = a / b;
             System.out.println(c);
@@ -13,8 +15,12 @@ public class error {
         } catch (ArithmeticException e) {
             System.out.println(e);
             System.out.println("Division by zero");
+        } catch (Exception e) {
+            System.out.println(e);
         }
-        // It can be use to avoid risk of exception while user is using program
+
+        // It is very important that we should write super classs of exception catch
+        // block to write in the end or else the control flow won't get into sub catch
 
     }
 
