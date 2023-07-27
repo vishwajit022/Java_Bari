315c9750c9dc2e990264fbb3effbed514ba5b0b2
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2023-07-27 16:46:20 +0530

    Update trywithresources.java

--- ErrorHandling/trywithresources.java
+++ ErrorHandling/trywithresources.java
@@ -6,13 +6,31 @@ import java.util.*;
 public class trywithresources {
 
     static FileInputStream f;
+    static Scanner sc;
 
-   static void Divide(){
+   static void Divide() throws Exception{
    f=new FileInputStream("C:\Users\vishw\Documents\GitHub\Java_Bari\ErrorHandling\my.txt");
-   Scanner sc=new Scanner(f);
+
+   // or if we don't want to go with hassle of closing the file then we can write  the above line in try(static FileInputStream f = new FileInputStream)
+   try{
+   sc=new Scanner(f);
+   int a= sc.nextInt();
+   int b=sc.nextInt();
+   int c=sc.nextInt();
+   System.out.println(a/b);
+   }
+finally{
+   f.close();
+   sc.close();
+}
    }
 
     public static void main(String arg[]) {
+
+        Divide();
+
+        // TODO: handle exception
+        System.out.println(e);
 
     }
 
