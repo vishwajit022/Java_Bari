136ec83831f9a5a7a5cea1959e1c4a1a3a3a3877
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2023-07-27 15:40:35 +0530

    go

---
+++ ErrorHandling/trywithresources.java
@@ -0,0 +1,19 @@
+package ErrorHandling;
+
+import java.io.*;
+import java.util.*;
+
+public class trywithresources {
+
+    static FileInputStream f;
+
+   static void Divide(){
+   f=new FileInputStream("C:\Users\vishw\Documents\GitHub\Java_Bari\ErrorHandling\my.txt");
+   Scanner sc=new Scanner(f);
+   }
+
+    public static void main(String arg[]) {
+
+    }
+
+}
