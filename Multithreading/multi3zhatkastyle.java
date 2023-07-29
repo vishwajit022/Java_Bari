fb283e7e60c04dcd381967d69a5950de54d3d85f
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2023-07-30 02:44:04 +0530

    Update multi3zhatkastyle.java

--- Multithreading/multi3zhatkastyle.java
+++ Multithreading/multi3zhatkastyle.java
@@ -4,6 +4,7 @@ public class multi3zhatkastyle extends Thread {
 
     // Why we don't write these methods as static why do we get error when we
     // declare it as static
+    // because it would break the contract and behavior of how threads work in Java.
     public void run() {
         while (true) {
             System.out.println("Run");
