8321ecbd0d958ebdb64e3b3576585ca621dd024f
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2023-08-01 05:51:00 +0530

    Create lang3wrapper.java

---
+++ Lang_Package/lang3wrapper.java
@@ -0,0 +1,34 @@
+package Lang_Package;
+
+public class lang3wrapper {
+    public static void main(String arg[]) {
+        // int i=10;
+        // Integer a= Integer.valueOf(10);
+        // Integer b=10;
+
+        // Byte c=15;
+        // Byte d=Byte.valueOf("15");
+
+        // //Boxing Method
+
+        // byte bb=15;
+        // Byte e=Byte.valueOf(bb);
+
+        // short s=15;
+        // Short ss =Short.valueOf(s);
+
+        // float f=12.3f;
+        // Float ff=Float.valueOf(f);
+
+        // char gg = 'S';
+        // Character g=Character.valueOf(gg);
+        // //This how we create Object of Primitive Data types
+
+        float f = 12.5f;
+        Float h = Float.valueOf(f);
+        float k = h.floatValue();
+        System.out.println(k);
+        // Autoboxing
+
+    }
+}
