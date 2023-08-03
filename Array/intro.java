831a67544be531cd3716273fa6e1263c110db37d
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2023-08-04 02:29:14 +0530

    adding comments to the cpde

--- Array/intro.java
+++ Array/intro.java
@@ -1,19 +1,23 @@
-package Array;
+package Array; // Declares the package name
 
-import java.util.*;
+import java.util.*; // Imports all classes from the java.util package
 
-public class intro {
+public class intro { // Defines a class named "intro"
 
-    public static void main(String args[]) { // Ways to declare arrays
-        int A[] = new int[10];
-        int B[] = { 10, 3, 4, 56, 6 };
-        int C[];
-        C = new int[10];
+    public static void main(String args[]) { // Main method, the entry point of the program
 
-        // Tranversing an array
+        // Ways to declare arrays
+        int A[] = new int[10]; // Declares and initializes an integer array "A" with a size of 10
+        int B[] = { 10, 3, 4, 56, 6 }; // Declares and initializes an integer array "B" with elements 10, 3, 4, 56, and
+                                       // 6
+        int C[]; // Declares an integer array reference "C"
+        C = new int[10]; // Initializes the integer array "C" with a size of 10
 
-        for (int i = 0; i <= A.length; i++) {
-            System.out.println(B[i]);
+        // Traversing an array
+
+        for (int i = 0; i <= A.length; i++) { // Loop to traverse the array "A" (note the incorrect condition i <=
+                                              // A.length, it should be i < A.length)
+            System.out.println(B[i]); // Print the element at index "i" in the array "B"
         }
 
     }
--- Array/twoDArray.java
+++ Array/twoDArray.java
@@ -1,27 +1,26 @@
-package Array;
+package Array; // Declares the package name
 
-import java.util.*;
+import java.util.*; // Imports all classes from the java.util package
 
-public class twoDArray {
-    public static void main(String arg[]) {
+public class twoDArray { // Defines a class named "twoDArray"
+
+    public static void main(String arg[]) { // Main method, the entry point of the program
+
         // Ways to declare 2-d Array
-        int A[][] = new int[5][5];
-
-        int B[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-
-        int C[][];
-        C = new int[5][5];
-
-        int[] D[] = new int[5][5];
+        int A[][] = new int[5][5]; // Declares and initializes a 2D integer array "A" with dimensions 5x5
+        int B[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } }; // Declares and initializes a 2D integer array "B" with
+                                                               // specific elements
+        int C[][]; // Declares a 2D integer array reference "C"
+        C = new int[5][5]; // Initializes the 2D integer array "C" with dimensions 5x5
+        int[] D[] = new int[5][5]; // Declares and initializes a 2D integer array "D" with dimensions 5x5
 
         // Traversing an array
 
-        for (int i = 0; i < B.length; i++) {
-            for (int j = 0; j < B.length; j++) {
-                System.out.println(B[i][j]);
+        for (int i = 0; i < B.length; i++) { // Outer loop to traverse rows of the 2D array "B"
+            for (int j = 0; j < B[i].length; j++) { // Inner loop to traverse columns of each row in the 2D array "B"
+                System.out.print(B[i][j] + "\t"); // Print the element at row "i" and column "j" in the 2D array "B"
             }
-            System.out.println(" ");
-
+            System.out.println(" "); // Print a blank line after each row is traversed
         }
 
     }
