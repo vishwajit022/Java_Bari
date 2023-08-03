dfdaf212daedbb4ce735a480058014b1f138978c
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2023-08-04 02:32:04 +0530

    Update errorthrowthrows.java

--- ErrorHandling/errorthrowthrows.java
+++ ErrorHandling/errorthrowthrows.java
@@ -1,26 +1,27 @@
-package ErrorHandling;
+package ErrorHandling; // Declares the package name "ErrorHandling"
 
-public class errorthrowthrows {
+public class errorthrowthrows { // Defines a class named "errorthrowthrows"
 
-    static int fun1() {
-        return 10 / 0;
+    static int fun1() { // Defines a static method named "fun1" which returns an integer
+        return 10 / 0; // Attempting to divide by zero, which will cause an ArithmeticException to be
+                       // thrown
     }
 
-    static void fun2() {
-        fun1();
-
+    static void fun2() { // Defines a static method named "fun2"
+        fun1(); // Calls the "fun1" method
     }
 
-    static void fun3() {
-        fun2();
+    static void fun3() { // Defines a static method named "fun3"
+        fun2(); // Calls the "fun2" method
     }
 
-    public static void main(String arg[]) {
+    public static void main(String arg[]) { // Main method, the entry point of the program
 
         try {
-            fun3();
-        } catch (Exception e) {
-            System.out.println(e);
+            fun3(); // Calls the "fun3" method, triggering the chain of method calls and exception
+                    // handling
+        } catch (Exception e) { // Catches any exception that occurs during the execution of the methods
+            System.out.println(e); // Prints the exception information to the console
         }
     }
 
