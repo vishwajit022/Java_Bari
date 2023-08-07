b90db24bc42f51a6c9a06341a0fe42c38b038f81
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2023-08-08 02:31:03 +0530

    Update p1_class.java

---
+++ F1_JavaDoc/p1_class.java
@@ -0,0 +1,35 @@
+/**
+ *@author Vishwajit 
+ * @version 2.0
+ * @since 2023 
+ **/
+
+package F1_JavaDoc;
+
+public class p1_class {
+
+    /*
+     * @author Vishwajit
+     * 
+     */
+
+    static int a = 20;
+
+    /*
+     * Parameterized Constructor
+     * 
+     * @param Book name
+     */
+    public p1_class(String s) {
+    }
+
+    /*
+     * @param roll number of a student
+     * 
+     * @throws Throws Exception if the book isn't available,throws Exception
+     */
+    public void issue(int roll) throws Exception {
+
+    }
+
+}
