753662cfb710dae21edf60a1b869df25f2a28cca
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2023-08-11 21:29:58 +0530

    Update p5_hashing.java

---
+++ F5_Collections/p5_hashing.java
@@ -0,0 +1,11 @@
+package F5_Collections;
+
+import java.util.*;
+
+//Hashing we do  %10=n with the input data number and store it in n
+
+public class p5_hashing {
+    public static void main(String arg[]) {
+
+    }
+}
